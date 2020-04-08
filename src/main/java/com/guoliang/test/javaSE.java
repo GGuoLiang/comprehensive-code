f201a0c2cb4a6abc80e42acfa8a3a89d35b1d4a4
GGuoLiang <1033479338@qq.com>
2020-04-08 09:15:01 +0800

    volatile 面试题

---
+++ src/main/java/com/guoliang/test/javaSE.java
@@ -0,0 +1,17 @@
+package com.guoliang.test;
+
+import org.junit.Test;
+
+/**
+ * @Author GGuoLiang
+ * @Date 2020/4/3 9:08 下午
+ * @Version 1.0
+ */
+public class javaSE {
+    @Test
+    public  void test(){
+        int i = 1;
+        i = i++;
+        System.out.println(i);
+    }
+}
